54ee673d5ed8b359b7d4a190dc414a02721bae9f
benkkgit <benjamin.kirley@georgebrown.ca>
2021-02-04 21:41:52 -0500

    Update script.js
    updated AJAX mechanism for the client and conact page

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ function loadpage(e){
 e.preventDefault();
 $("#menu a.active").removeClass("active");
 $(this).addClass("active");
-$("#content").load("about.html"); 
+
+var href = $(this).attr("href");
+$("#content").load(href); 
 }
 
 $(document).on("click", "#menu a", loadpage);
